e13247a477fd050d640f1ecd52e44270202a5f50
Marclus Lopes <marcluslopes@gmail.com>
2018-10-06 15:07:42 +0530

    Added ResultSetMetaData

--- Java/Class/Database/DatabaseSelect.java
+++ Java/Class/Database/DatabaseSelect.java
@@ -12,7 +12,8 @@ class DatabaseSelect{
     try {
       PreparedStatement pst = con.prepareStatement(query);
       ResultSet rows = pst.executeQuery();
-
+      ResultSetMetaData meta = rows.getMetaData();
+      System.out.print(meta.getColumnCount());
       while(rows.next()){
         System.out.print(row.getString(1)+" "+row.getString(2)+" "+row.getString(3)+" ");
       }
